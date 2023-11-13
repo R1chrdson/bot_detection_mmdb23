24e9acdf5811a872c6203a8eae5b4634655e2659
Andrii Kapatsyn <prostoandrey01@gmail.com>
2023-11-14 00:19:38 +0200

    Added dataset sampling

--- fetch_data/parse.py
+++ fetch_data/parse.py
@@ -8,15 +8,29 @@ from helpers.graceful_killer import GracefulKiller
 
 DATASET_PATH = Path('data')
 
-killer = GracefulKiller()
 
-stream = EventStreams(streams=['recentchange'], since='2023-11-07T12:43:00Z')
-stream.register_filter(server_name='en.wikipedia.org')
+def sample_data_stream(change: dict, keep_percent:int = 20) -> bool:
+    """
+    Whether to keep the change in the dataset according to the sampling strategy
+    """
+    return hash(change['user']) % 100 > (99 - keep_percent)
 
-with tqdm() as pbar:
-    with open(DATASET_PATH / 'dataset.jsonl', 'a') as outfile:
-        while not killer.kill_now:
-            change = next(stream)
-            json.dump(change, outfile)
-            outfile.write('\n')
-            pbar.update()
+
+def main():
+    killer = GracefulKiller()
+
+    stream = EventStreams(streams=['recentchange'], since='2023-10-07T12:43:00Z')
+    stream.register_filter(server_name='en.wikipedia.org')
+    stream.register_filter(sample_data_stream)
+
+    with tqdm() as pbar:
+        with open(DATASET_PATH / 'dataset.jsonl', 'a') as outfile:
+            while not killer.kill_now:
+                change = next(stream)
+                json.dump(change, outfile)
+                outfile.write('\n')
+                pbar.update()
+
+
+if __name__ == "__main__":
+    main()
